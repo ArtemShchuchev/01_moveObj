4d0a7fbdb7e801249e12d7726fbbe551532e8a9d
ArtemShchuchev <shuaa@rambler.ru>
2023-01-14 10:47:21 +0700

    Add files

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "SecondFunk/SecondaryFunction.h"
+
+/*
+Задача 1. Перемещение объектов
+Описание
+Вам нужно реализовать шаблонную функцию, которая перемещает содержимое одного std::vector в другой.
+
+std::vector <std::string> one = { "test_string1", "test_string2" };
+std::vector <std::string> two;
+move_vectors(...)
+*/
+
+int main(int argc, char** argv)
+{
+	printHeader("Задача 1. Перемещение объектов");
+	
+	std::vector <std::string> one = { "test_string1", "test_string2" };
+	std::vector <std::string> two;
+	move_vectors(...)
+
+	return 0;
+}
